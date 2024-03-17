7c8b13c2df4a84417298c35b5969634b3ea07c21
Gosia <sobocinska.malgo@gmail.com>
2024-03-17 10:49:58 +0100

    new framework partII

--- src/test/java/newFramework/base/NewFrameworkTestBase.java
+++ src/test/java/newFramework/base/NewFrameworkTestBase.java
@@ -2,6 +2,11 @@ package newFramework.base;
 
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import newFramework.client.ApiClient;
 import newFramework.client.JacksonMapper;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
@@ -13,4 +18,12 @@ public class NewFrameworkTestBase {
     protected static WireMockExtension mock = WireMockExtension.newInstance().options(wireMockConfig().dynamicPort()).build();
 
     protected static ObjectWriter writer = new JacksonMapper().create().writer().withDefaultPrettyPrinter();
+
+    protected ApiClient createApiClient(String baseUri){
+        return new ApiClient(()-> new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .addFilter(new RequestLoggingFilter())
+                .addFilter(new ResponseLoggingFilter())
+                .setBaseUri(baseUri));
+    }
 }
--- src/test/java/newFramework/test/NewFrameworkStudentApiTest.java
+++ src/test/java/newFramework/test/NewFrameworkStudentApiTest.java
@@ -1,11 +1,29 @@
 package newFramework.test;
 
-import org.junit.Test;
+import newFramework.base.NewFrameworkTestBase;
+import newFramework.client.ApiClient;
+import org.eclipse.jetty.http.HttpStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-public class NewFrameworkStudentApiTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NewFrameworkStudentApiTest extends NewFrameworkTestBase {
+
+    private final String baseUri = "https://thetestingworldapi.com";
+    private ApiClient api;
+
+    @BeforeEach
+    public void setUpClient(){
+        api = createApiClient(baseUri);
+    }
+
+    //10093344 //10093375 //10093384
 
     @Test
     public void shouldGetStudent(){
-
+        assertThat( api.getRealStudent("10093344")
+                .execute()
+                .getStatusCode()).isEqualTo(HttpStatus.OK_200);
     }
 }
